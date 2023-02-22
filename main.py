71f02773f677dee48aa0c6c571927175acfadd0b
Philipp Wundrack <philipp.wundrack@live.de>
2023-02-22 09:48:42 +0100

    add setup and teardown

--- main.py
+++ main.py
@@ -9,70 +9,73 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestPluginExecution(unittest.TestCase):
+	def setUp(self) -> None:
+		self.driver = webdriver.Firefox()
+
+	def tearDown(self) -> None:
+		self.driver.close()
+
+	def test_create_new_experiment(self):
+		pass
+
 	def test_hello_world_multi_step(self):
-		driver = webdriver.Firefox()
-		driver.get("http://localhost:8080")
-		driver.implicitly_wait(10)
+		self.driver.get("http://localhost:8080")
+		self.driver.implicitly_wait(10)
 
-		try:
-			new_experiment_button = driver.find_element(By.XPATH, "//button[span[text()='New Experiment']]")
-			new_experiment_button.click()
+		new_experiment_button = self.driver.find_element(By.XPATH, "//button[span[text()='New Experiment']]")
+		new_experiment_button.click()
 
-			driver.switch_to.active_element.send_keys("test")
+		self.driver.switch_to.active_element.send_keys("test")
 
-			create_experiment_button = driver.find_element(By.XPATH, "//button[span[normalize-space(text())='Create Experiment']]")
-			create_experiment_button.click()
+		create_experiment_button = self.driver.find_element(By.XPATH, "//button[span[normalize-space(text())='Create Experiment']]")
+		create_experiment_button.click()
 
-			workspace_tab = driver.find_element(By.XPATH, "//a[span[normalize-space(text())='Workspace']]")
-			workspace_tab.click()
+		workspace_tab = self.driver.find_element(By.XPATH, "//a[span[normalize-space(text())='Workspace']]")
+		workspace_tab.click()
 
-			time.sleep(1)
-			hello_world_multi_step_list_item = driver.find_element(By.XPATH, "//span[starts-with(text(), 'hello-world-multi-step')]")
+		time.sleep(1)
+		hello_world_multi_step_list_item = self.driver.find_element(By.XPATH, "//span[starts-with(text(), 'hello-world-multi-step')]")
 
-			hello_world_multi_step_list_item.click()
+		hello_world_multi_step_list_item.click()
 
-			frontend_iframe = driver.find_element(By.XPATH, "//iframe[@class='frontend-frame']")
-			driver.switch_to.frame(frontend_iframe)
+		frontend_iframe = self.driver.find_element(By.XPATH, "//iframe[@class='frontend-frame']")
+		self.driver.switch_to.frame(frontend_iframe)
 
-			input_field = driver.find_element(By.XPATH, "//textarea[@name='inputStr']")
-			input_field.send_keys("input text")
+		input_field = self.driver.find_element(By.XPATH, "//textarea[@name='inputStr']")
+		input_field.send_keys("input text")
 
-			submit_button = driver.find_element(By.XPATH, "//button[@class='qhana-form-submit'][text()='submit']")
-			submit_button.click()
+		submit_button = self.driver.find_element(By.XPATH, "//button[@class='qhana-form-submit'][text()='submit']")
+		submit_button.click()
 
-			driver.switch_to.default_content()
+		self.driver.switch_to.default_content()
 
-			substep1_iframe = WebDriverWait(driver, timeout=10).until(self._get_frontend_iframe)
-			driver.switch_to.frame(substep1_iframe)
+		substep1_iframe = WebDriverWait(self.driver, timeout=10).until(self._get_frontend_iframe)
+		self.driver.switch_to.frame(substep1_iframe)
 
-			time.sleep(1)
-			substep1_submit_button = driver.find_element(By.XPATH, "//button[@class='qhana-form-submit'][text()='submit']")
-			substep1_submit_button.click()
+		time.sleep(1)
+		substep1_submit_button = self.driver.find_element(By.XPATH, "//button[@class='qhana-form-submit'][text()='submit']")
+		substep1_submit_button.click()
 
-			driver.switch_to.default_content()
+		self.driver.switch_to.default_content()
 
-			WebDriverWait(driver, timeout=10).until(self._check_if_finished)
+		WebDriverWait(self.driver, timeout=10).until(self._check_if_finished)
 
-			output_tab = driver.find_element(By.XPATH, "//a[normalize-space(text())='Outputs']")
-			output_tab.click()
+		output_tab = self.driver.find_element(By.XPATH, "//a[normalize-space(text())='Outputs']")
+		output_tab.click()
 
-			preview_iframe1 = driver.find_element(By.XPATH, "//iframe[contains(@src, 'output1.txt')]")
-			driver.switch_to.frame(preview_iframe1)
+		preview_iframe1 = self.driver.find_element(By.XPATH, "//iframe[contains(@src, 'output1.txt')]")
+		self.driver.switch_to.frame(preview_iframe1)
 
-			output_text1 = driver.find_element(By.XPATH, "//pre")
-			assert output_text1.text == "Processed in the preprocessing step: input text"
-			driver.switch_to.default_content()
+		output_text1 = self.driver.find_element(By.XPATH, "//pre")
+		assert output_text1.text == "Processed in the preprocessing step: input text"
+		self.driver.switch_to.default_content()
 
-			preview_iframe2 = driver.find_element(By.XPATH, "//iframe[contains(@src, 'output2.txt')]")
-			driver.switch_to.frame(preview_iframe2)
+		preview_iframe2 = self.driver.find_element(By.XPATH, "//iframe[contains(@src, 'output2.txt')]")
+		self.driver.switch_to.frame(preview_iframe2)
 
-			output_text2 = driver.find_element(By.XPATH, "//pre")
-			assert output_text2.text == "Processed in the processing step: input text Input from preprocessing: input text"
-			driver.switch_to.default_content()
-		except Exception as e:
-			raise e
-		finally:
-			driver.close()
+		output_text2 = self.driver.find_element(By.XPATH, "//pre")
+		assert output_text2.text == "Processed in the processing step: input text Input from preprocessing: input text"
+		self.driver.switch_to.default_content()
 
 	@staticmethod
 	def _get_frontend_iframe(driver: WebDriver) -> WebElement:
